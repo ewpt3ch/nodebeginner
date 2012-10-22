b38adb19ffcee17c82dc25cf9d2b17074c3fd33d
Eric Phillips <ewphillips@msn.com>
2012-10-22 01:26:41 -0400

    a

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var http = require("http");
 
-http.createServer(function(request, response) {
-    console.log("request received");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-}).listen(8888);
+function start() {
+    function onRequest(request, response) {
+        console.log("request received");
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World");
+        response.end();
+    }
+    
+    http.createServer(onRequest).listen(8888);
+    console.log("server has started");
+    
+}
 
-console.log("server has started");
+exports.start = start;
