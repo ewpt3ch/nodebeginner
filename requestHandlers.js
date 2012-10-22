d22a8fed51774970ca8de5af5e1a9c1e16d3e05e
Eric Phillips <ewphillips@msn.com>
2012-10-22 04:40:15 -0400

    can see what was upload

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,6 @@
 var exec = require("child_process").exec;
 
-function start(response) {
+function start(response, postData) {
     console.log("Request handler start was called");
 
     var body = '<html>'+
@@ -21,24 +21,10 @@ function start(response) {
     response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
     console.log("Request handler upload was called");
-        
-    var body = '<html>'+
-    '<head>'+
-    '<meta http-equiv="Content-Type" content="text/html; '+
-    'charset=UTF-8"/>'+
-    '</head>'+
-    '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Submit text"/>'+
-    '</form>'+
-    '</body>'+
-    '</html>';
-    
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write(body);
+    response.write("You sent" + postData);
     response.end();
 }
 
